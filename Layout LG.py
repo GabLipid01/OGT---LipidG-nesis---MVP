9c3bd4f8066897c4f327de3b3966074f7500625e
GabLipid01 <acessogwjp@gmail.com>
2025-04-29 14:55:52 -0300

    Update Layout LG.py

--- Layout LG.py
+++ Layout LG.py
@@ -1,6 +1,5 @@
-st.set_page_config(page_title="LipidGenesis - Blend LG", layout="wide")
-
 import streamlit as st
+st.set_page_config(page_title="LipidGenesis - Blend LG", layout="wide")
 import pandas as pd
 import plotly.express as px
 from fpdf import FPDF
