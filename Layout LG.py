01011f9279ce4d28ed5b540ed25721bab2c4088a
GabLipid01 <acessogwjp@gmail.com>
2025-04-29 13:21:14 -0300

    Update Layout LG.py

--- Layout LG.py
+++ Layout LG.py
@@ -68,8 +68,6 @@ else:
         for fa in all_fatty_acids
     }
 
-st.set_page_config(page_title="LipidGenesis - Blend LG", layout="wide")
-
 # === Título principal ===
 st.title("🌿 LipidGenesis - Bioengineering Of Oils For Nextgen")
 st.markdown("<h3 style='text-align: center; color: #4C9B9C;'>PLATAFORMA DE FORMULAÇÃO PERSONALIZADA DE BLENDS DE PALMA E PALMISTE</h3>", unsafe_allow_html=True)
