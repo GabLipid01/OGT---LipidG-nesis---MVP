981ade1defdfd26a50993173fb2f280446c503c2
GabLipid01 <acessogwjp@gmail.com>
2025-09-03 22:31:52 -0300

    blend_enzimatico.py

--- blend_enzimatico.py
+++ blend_enzimatico.py
@@ -156,7 +156,7 @@ def iodine_index(fa_pct: dict) -> float:
     return sum((fa_pct.get(k, 0.0) / 100.0) * FA_CONST[k]["IV"] for k in FA_CONST.keys())
 
 def saponification_index(fa_pct: dict) -> float:
-    return sum((fa_pct.get(k, 0.0) / 100.0) * (560.0 / FA_CONST[k]["MW"]) for k in FA_CONST)
+    return sum(fa_pct.get(k, 0.0) * (560.0 / FA_CONST[k]["MW"]) for k in FA_CONST.keys())
 
 # ----------------- Heurísticas sensoriais -----------------
 def _spread(fa):  # espalhabilidade
